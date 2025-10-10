2ee428c5d66350e567a6d42694d82576eb415e8a
Alexander LeFeber <alefeber@iastate.edu>
2025-10-09 19:49:51 -0500

    Added DELETE, a clear button (can be kept for an admin user or something), mainly to clear out test items created in the sell menu. Added buy button to each item, simple functionality of reducing quantity until 0 for now.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/Listing.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/Listing.java
@@ -5,12 +5,26 @@ public class Listing {
     public String description;
     public double price;
     public int quantity;
+    private int id;
 
     // represents a single item listing in marketplace
-    public Listing(String title, String description, double price, int quantity) {
+    public Listing(String title, String description, double price, int quantity, int id) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int q) {
+        this.quantity = q;
     }
 }
