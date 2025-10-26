164552633ee21f9015935ae67b015a8c2cfcf9b1
Tyler Mestery <tyler@mestery.com>
2025-10-25 21:35:47 -0500

    Created favorites activity and added heart icon from google icons for it. Also updated messages screen to be prepped for setting up websockets (UI). Added navigation bar to both favorites and messages screen, reducing amount of buttons and increasing overall UI presentationn.

--- Frontend/CyMarket/app/src/main/java/com/example/cymarket/FavoritesActivity.java
+++ Frontend/CyMarket/app/src/main/java/com/example/cymarket/FavoritesActivity.java
@@ -1,33 +1,30 @@
 package com.example.cymarket;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MessagesActivity extends AppCompatActivity {
+public class FavoritesActivity extends AppCompatActivity {
+    private Button clearButton;      // define clear button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messages);
+        setContentView(R.layout.activity_buy);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id == R.id.nav_buy) {
-                startActivity(new Intent(MessagesActivity.this, BuyActivity.class));
+                startActivity(new Intent(FavoritesActivity.this, BuyActivity.class));
                 return true;
             } else if (id == R.id.nav_sell) {
-                startActivity(new Intent(MessagesActivity.this, SellActivity.class));
+                startActivity(new Intent(FavoritesActivity.this, SellActivity.class));
                 return true;
             } else if (id == R.id.nav_chat) {
-                startActivity(new Intent(MessagesActivity.this, MessagesActivity.class));
+                startActivity(new Intent(FavoritesActivity.this, MessagesActivity.class));
                 return true;
             }
             return false;
